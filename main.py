bdd6dcc50b652327a0df21e95eb6af4f3f8d82f4
shambu <shambuqwerty@gmail.com>
2021-06-27 15:12:02 +0530

    Plotted the final route with animation

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import numpy as np
 import plotly.express as px
 from sklearn.cluster import KMeans
-from testServer import Server
 from log import Logger
 from queue import Queue
+import plotly.graph_objects as go
 
 Logger.clear()
 log = Logger.log
@@ -26,11 +26,11 @@ kmeans = KMeans(n_clusters=10, random_state=0).fit(training)
 p_d = np.concatenate([pos, dest], axis=0)
 color = np.concatenate([np.zeros(n), np.ones(n)], axis=0)
 text = kmeans.labels_
-fig = px.scatter(
-    x=p_d[:, 0],
-    y=p_d[:, 1],
-    color=text,
-    hover_name=["Start" if i == 0 else "Destination" for i in color])
+fig = px.scatter(x=p_d[:, 0],
+                 y=p_d[:, 1],
+                 color=color,
+                 hover_name=text,
+                 title="Taxi Routing Based on Passenger Destination")
 
 classes = kmeans.predict(training1).reshape((-1, 1))
 
@@ -42,21 +42,22 @@ curr_center = np.concatenate(
         10, 1), np.ones((10, 1))),
     axis=1)
 
-curr_passengers = np.concatenate(
-    (training1[:, 0:2], np.ones((training1.shape[0], 1))), axis=1)
+curr_passengers = np.concatenate((training1, np.ones((training1.shape[0], 1))),
+                                 axis=1)
 
 
+#*------------------------------Helper funcs-------------------------------------#
 def getSameClass(_class, classes, data):
     """
     *get passengers with same class
     @param _class  -> int
     @param classes -> vector :: (num_samples, 1)
-    @param data    -> vector :: (num_samples, 3)
+    @param data    -> vector :: (num_samples, 5)
     """
     _class = int(_class)
-    assert(isinstance(_class, int))
-    assert(classes.shape[1] == 1)
-    assert(data.shape[1] == 3)
+    assert (isinstance(_class, int))
+    assert (classes.shape[1] == 1)
+    assert (data.shape[1] == 5)
 
     mask = classes == _class
     return data[mask.squeeze()]
@@ -68,13 +69,13 @@ def getNearestCentreClass(centers, driver):
     @param centers -> vector :: (10, 6)
     @param driver  -> vector :: (1, 2)
     """
-    driver = driver.reshape((1,2))
-    assert(centers.shape[1] == 6)
-    assert(driver.shape == (1, 2))
+    driver = driver.reshape((1, 2))
+    assert (centers.shape[1] == 6)
+    assert (driver.shape == (1, 2))
 
     mask = centers[:, -1] == 1
     tmp = centers[mask]
-    assert(tmp.shape[1] == centers.shape[1])
+    assert (tmp.shape[1] == centers.shape[1])
 
     dis = np.linalg.norm(tmp[:, 0:2].copy() - driver)
     mini = np.argmin(dis)
@@ -88,41 +89,36 @@ def getNearestPassenger(driver, passengers):
     """
     *get nearest passenger in the same class
     @param driver      -> vector :: (1, 2)
-    @param passengers  -> vector :: (nums_samples, 3)
+    @param passengers  -> vector :: (nums_samples, 5)
     """
-    driver = driver.reshape((1,2))
-    assert(driver.shape==(1, 2))
-    assert(passengers.shape[1]==3)
+    driver = driver.reshape((1, 2))
+    assert (driver.shape == (1, 2))
+    assert (passengers.shape[1] == 5)
 
     mask = passengers[:, -1] == 1
     tmp = passengers[mask]
 
-    dis = np.linalg.norm(tmp[:,0:2].copy() - driver)
+    dis = np.linalg.norm(tmp[:, 0:2].copy() - driver)
     mini = np.argmin(dis)
-    tmp[mini, 2] = 0
+    tmp[mini, -1] = 0
     passengers[mask] = tmp
-    return tmp[mini, 0:2], passengers, tmp.shape[0] - 1
+    return tmp[mini, 0:4], passengers, tmp.shape[0] - 1
 
 
-log("\n")
-log("centers")
-assert (len(curr_center) == 10)
-log(curr_center)
-log(curr_center.shape)
+#*------------------------------------------------------------------------------#
 
+"""
+#!-------------------------------taxi starting----------------------------------#
 #check driver belongs to which  cluster
 driver = np.array([0, 0])
 assert (driver.shape == curr_passengers[0, 0:2].shape)
 
 queue = []
-queue.append(driver)
-
 tmp_passengers = curr_passengers
 k = 1
 traversed = 0
 f = 0
-#------------------------------------------------------#
-#!taxi starting!
+
 while (traversed < 10):
     f += 1
     if f == 1000:
@@ -135,20 +131,67 @@ while (traversed < 10):
     while (k != 0):
         tmp_driver, tmp_passengers, k = getNearestPassenger(
             driver, tmp_passengers)
-        queue.append(tmp_driver)
-        driver = tmp_driver
+        queue.append((tmp_driver[0:2], tmp_driver[2:]))
+        driver = tmp_driver[0:2]
 
-   
     k = 1
     traversed += 1
+np.save("data/queue", queue)
+#!-------------------------------------------------------------------------------#
+"""
 
-log("\n")
-log(queue)
-log(len(queue))
-#-------------------------------------------------------#
+q = np.load("data/queue.npy")
+q = q.reshape((-1, 4))
+x = []
+y = []
+for i in range(20):
+    x.append(q[i, 0])
+    y.append(q[i, 1])
+    x.append(q[i, 2])
+    y.append(q[i, 3])
+
+tmp = [0]
+tmp2 = [0]
+
+
+def append(tmp, el):
+    tmp.append(el)
+    return tmp
+
+
+frames = [
+    go.Frame(data=[go.Scatter(x=append(tmp, x[i]), y=append(tmp2, y[i]))])
+    for i in range(40)
+]
+
+fig2 = go.Figure(
+    data=[go.Scatter(x=[0], y=[0], line=dict(color="rgb(189,189,189)"))],
+    layout=go.Layout(
+        xaxis=dict(range=[0, 400], autorange=False),
+        yaxis=dict(range=[0, 400], autorange=False),
+        title="Taxi Routing Based on Passenger Destination",
+        updatemenus=[
+            dict(type="buttons",
+                 buttons=[dict(label="Play", method="animate", args=[None])])
+        ]),
+    frames=frames)
+
+fig.update_traces(marker_coloraxis=None)
+fig2.add_trace(fig.data[0])
+fig3 = go.Figure(
+    data=go.Scatter(x=x, y=y, line=dict(color="rgb(189,189,189)")),
+    layout=go.Layout(xaxis=dict(range=[-10, 400], autorange=False),
+                     yaxis=dict(range=[-10, 400], autorange=False),
+                     title="Taxi Routing Based on Passenger Destination"))
+
+fig3.update_layout(xaxis_title="x", yaxis_title="y")
+fig3.add_trace(fig.data[0])
+fig3.update_traces(showlegend=False)
+
+fig2.update_traces(showlegend=False)
+log(q)
 
 if __name__ == '__main__':
-    # app = Server()
-    # app(fig)
-    # app.run()
-    pass
+    fig3.write_image("animation/final.png")
+    fig.write_image("animation/plot.png")
+    fig2.show()
